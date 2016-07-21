2087e54e9612015921d2989a28e85b6b43835648
Kiritoinder <inderbharaj@ymail.com>
2016-07-21 15:48:01 -0400

    Added grade.txt file with exception handling

--- Assignment_5/Program.cs
+++ Assignment_5/Program.cs
@@ -21,9 +21,9 @@ namespace Assignment5
             string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
             const char DELIM = ',';
 
-            int selection = 0;
+            int choice = 0;
 
-            while (selection != 2)
+            while (choice != 2)
             {
                 Console.WriteLine("|=======Student Grades=======|");
                 Console.WriteLine("| 1) Display Grades          |");
@@ -33,13 +33,101 @@ namespace Assignment5
 
                 try
                 {
-                    selection = Convert.ToInt32(Console.ReadLine());
+                    choice = Convert.ToInt32(Console.ReadLine());
                 }
                 catch (Exception error)
                 {
                     Console.WriteLine(error.Message);
-                    selection = 0;
+                    choice = 0;
                 }
+                switch (choice)
+                {
+                    case 1:
+                        gradeTxt(path, DELIM);
+                        ShowGrade(path, DELIM);
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Inavlid selection. Please Choose a valid option\n");
+                        break;
+                }
+            }
+        }
+
+        // Create grade.txt file
+        private static void gradeTxt(string path, char DELIM)
+        {
+            string fileName = "grade.txt";
+            string[] firstName = { "Inderjeet", "Abhinav", "Iqbal" };
+            string[] lastName = { "Singh", "Shardha", "gill" };
+            string[] studentID = { "1", "2", "3" };
+            string[] subject = { "Methodology", "Programming", "Database" };
+            string[] grade = { "A+", "B", "A" };
+
+            try
+            {
+                FileStream outFile = new FileStream(path + "\\" + fileName, FileMode.Create, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(outFile);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    writer.WriteLine(firstName[i] + DELIM + lastName[i] + DELIM + studentID[i] + DELIM + subject[i] + DELIM + grade[i]);
+                }
+                writer.Close();
+                outFile.Close();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error -> {0}", error.Message);
+            }
+        }
+
+        // Display grade
+        public static void ShowGrade(string pathName, char DELIM)
+        {
+            string fileName;
+            string[] fields;
+            string fileData = "";
+            string firstName;
+            string lastName;
+            string studentIndex;
+            string className;
+            string classGrade;
+
+            Console.Write("Please enter file name to load --> ");
+            fileName = Console.ReadLine();
+            Console.WriteLine();
+
+            try
+            {
+                FileStream inFile = new FileStream(pathName + "\\" + fileName, FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(inFile);
+
+                fileData = reader.ReadLine();
+
+                while (fileData != null)
+                {
+                    fields = fileData.Split(DELIM);
+                    firstName = fields[0];
+                    lastName = fields[1];
+                    studentIndex = fields[2];
+                    className = fields[3];
+                    classGrade = fields[4];
+
+                    Console.WriteLine("{0}, {1}: {2} {3}, {4}", firstName, lastName, studentIndex, className, classGrade);
+                    fileData = reader.ReadLine();
+                }
+                Console.WriteLine();
+                reader.Close();
+                inFile.Close();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("File Not Found");
+                Console.WriteLine(error.Message);
+                Console.WriteLine();
             }
         }
         // Exiting ++++++++++++++++++++++++++++++++++++++
