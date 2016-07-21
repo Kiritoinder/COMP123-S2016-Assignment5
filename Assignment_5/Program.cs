604846999204ebd7f258ac6638488cc27f3167ad
Kiritoinder <inderbharaj@ymail.com>
2016-07-21 15:37:49 -0400

    Added Menu and Exit

--- Assignment_5/Program.cs
+++ Assignment_5/Program.cs
@@ -11,6 +11,46 @@ namespace Assignment5
     class Program
     {
         static void Main(string[] args)
-        { }
+        {
+            Menu();
+            Exit();
+        }
+
+        public static void Menu()
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            const char DELIM = ',';
+
+            int selection = 0;
+
+            while (selection != 2)
+            {
+                Console.WriteLine("|=======Student Grades=======|");
+                Console.WriteLine("| 1) Display Grades          |");
+                Console.WriteLine("| 2) Exit                    |");
+                Console.WriteLine("|============================|");
+                Console.Write("Please Select from above options --> ");
+
+                try
+                {
+                    selection = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                    selection = 0;
+                }
+            }
+        }
+        // Exiting ++++++++++++++++++++++++++++++++++++++
+        public static void Exit()
+        {
+            Console.WriteLine();
+            Console.WriteLine("|================================|");
+            Console.WriteLine("|Bye ----> |Press any key to exit|");
+            Console.WriteLine("|================================|");
+            Console.ReadKey();
+            Console.Clear();
+        }
     }
 }
