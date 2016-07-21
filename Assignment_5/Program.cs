b9e2a40e21e3e36644da512453fffada3da793e3
Kiritoinder <inderbharaj@ymail.com>
2016-07-21 15:25:46 -0400

    Initial Commit

--- Assignment_5/Program.cs
+++ Assignment_5/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Assignment_5
+namespace Assignment5
 {
     class Program
     {
         static void Main(string[] args)
-        {
-        }
+        { }
     }
 }
---
+++ Assignment_5/student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    class student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string subjet { get; set; }
+
+        public string classGrade { get; set; }
+    }
+}
